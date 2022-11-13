8cffdee6d1294dd05950112f1831859031d34781
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-13 23:52:23 +0530

    Add: Total employee wage for a month

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -1,26 +1,46 @@
 package com.bl;
 
 public class EmployeeWage {
+    static final int IS_FULL_TIME=2;
+    static final int IS_PART_TIME=1;
+    static final int WAGE_PER_HOUR=20;
+    static final int FULL_DAY_HOUR=10;
+    static final int PART_TIME_HOUR=5;
+    public static void main(String[] args) {
+        int attendance;
+        int dailyWage=0;
+        int day=1;
+        int totalWage=0;
 
-    static int WAGE_PER_HOUR=20;
-    static int FULL_DAY_HOUR=10;
-        public static void main(String[] args) {
-            int attendance;
-            int dailyWage = 0;
-            System.out.println("Welcome to Employee Wage Computation");
-            attendance=checkAttendance();
+        System.out.println("Welcome to Employee Wage Computation");
 
-            if (attendance == 1) {
-                System.out.println("Employee is Present");
-              dailyWage = WAGE_PER_HOUR*FULL_DAY_HOUR;
-            }else
-                System.out.println("Employee is Absent");
+        while (day <=20) {
+            System.out.println("Day #"+day);
+            attendance = checkAttendance();
+
+            switch (attendance) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is full time");
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    totalWage+=dailyWage;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is part time");
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    totalWage+=dailyWage;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    break;
+            }
 
             System.out.println("Daily Wage = " + dailyWage);
+            day++;
         }
-
-        static int checkAttendance(){
-            double random = ((Math.random()*10)%2);
-            return (int) random;
-        }
+        System.out.println("Total Wage of month= "+totalWage);
+    }
+    static int checkAttendance(){
+        int random=(int)(Math.random()*10)%3;
+        return random;
+    }
 }
