73f8bd3f249a43b289087dd78bf38522bb197bf7
Pradhyankita Bhasme <68118579+Pradhyankita@users.noreply.github.com>
2022-11-16 16:06:39 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,10 @@
+package com.bl;
+public class App {
+    /**
+     * Hello world!
+   **/
+    public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+}
